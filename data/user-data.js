65baf4eecef4e39a97727057d6db44343d81cd11
Def4l71diot <lubeto3099@gmail.com>
2016-11-28 02:29:56 +0200

    Fixed bad error message

--- data/user-data.js
+++ data/user-data.js
@@ -1,6 +1,9 @@
 /* globals require module  */
 /* jshint esversion: 6 */
 
+const userExistsErrorCode = 11000;
+const userExistsErrorMessage = "User with this username aleready exists!";
+
 module.exports = function(models) {
     let { User } = models;
 
@@ -10,6 +13,10 @@ module.exports = function(models) {
             return new Promise((resolve, reject) => {
                 user.save(err => {
                     if (err) {
+                        if (err.code === userExistsErrorCode) {
+                            return reject(userExistsErrorMessage);
+                        }
+
                         return reject(err);
                     }
 
