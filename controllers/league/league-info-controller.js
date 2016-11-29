5f36ea9c4798842ed960c8bb37757c15b92a0bfd
Kalito98 <Kaloyan.Yanev98@gmail.com>
2016-11-29 17:07:40 +0200

    Added skeleton for game info tab

--- controllers/league/league-info-controller.js
+++ controllers/league/league-info-controller.js
@@ -2,6 +2,9 @@ module.exports = function() {
     return {
         getSummonerInfo(req, res) {
             return res.render('league-info/summonerinfo');
+        },
+        getGameInfo(req, res) {
+            return res.render('league-info/gameinfo');
         }
     };
 };
