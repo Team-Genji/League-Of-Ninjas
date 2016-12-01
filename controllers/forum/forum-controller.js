b1c898ca4123adf6d7bce210294fde0eb5443128
ultron1251 <alexander.yankov1173@abv.bg>
2016-12-01 17:53:51 +0200

    small forum controller error fixed

--- controllers/forum/forum-controller.js
+++ controllers/forum/forum-controller.js
@@ -4,7 +4,7 @@ module.exports = function (data) {
             return data.getForums()
                 .then(forums => {
                     res.render('./forums/forum-main', {
-                        models: forums,
+                        forums,
                         user: req.user
                     });
                 })
