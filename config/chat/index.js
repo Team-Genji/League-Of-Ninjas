1987cda6a4c3ff0e0cb1ec09fd812869fbb7ce6d
Def4l71diot <lubeto3099@gmail.com>
2016-12-03 22:21:42 +0200

    Added XSS-prevention for chat

--- config/chat/index.js
+++ config/chat/index.js
@@ -1,9 +1,11 @@
+const xssFilters = require('xss-filters');
 module.exports = function(server) {
     let io = require('socket.io')(server);
 
     io.sockets.on('connection', socket => {
         socket.emit('message', { message: 'Welcome to the chat' });
         socket.on('send', dataSocket => {
+            dataSocket.message = xssFilters.inHTMLData(dataSocket.message);
             io.sockets.emit('message', dataSocket);
         });
     });
